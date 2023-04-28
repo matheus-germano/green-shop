b5083d2c7c1668a6dea19c6ee13b65b3bb3502ad
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-04-28 19:43:33 -0300

    :tada: initial commit

---
+++ src/main/java/com/matheusgermano/greenshop/GreenShopApplication.java
@@ -0,0 +1,13 @@
+package com.matheusgermano.greenshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenShopApplication.class, args);
+	}
+
+}
