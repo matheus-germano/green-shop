243834cd1458ceacdec9ee85d202da23a2dcbe12
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-04-28 22:07:07 -0300

    :sparkles: feat: findByInCountryId and findByEmail users repository methods

--- src/main/java/com/matheusgermano/greenshop/repositories/UsersRepository.java
+++ src/main/java/com/matheusgermano/greenshop/repositories/UsersRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface UsersRepository extends JpaRepository<User, UUID> {
+    User findByInCountryId(String inCountryId);
+    User findByEmail(String email);
 }
