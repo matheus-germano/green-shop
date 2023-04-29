1aeb3cea5672981da732e6ccc59d49dc16d684a0
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-04-28 21:22:12 -0300

    :bug: fix: user model

--- src/main/java/com/matheusgermano/greenshop/models/User.java
+++ src/main/java/com/matheusgermano/greenshop/models/User.java
@@ -18,4 +18,44 @@ public class User {
     private String email;
     @Column(nullable = false)
     private String password;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getInCountryId() {
+        return inCountryId;
+    }
+
+    public void setInCountryId(String inCountryId) {
+        this.inCountryId = inCountryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
