e369c7d7a12c64282abcd9b7ce93dedb80bb3f85
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-04-29 14:57:36 -0300

    :sparkles: feat: verify if token is valid and get id inside token methods

--- src/main/java/com/matheusgermano/greenshop/util/JwtUtil.java
+++ src/main/java/com/matheusgermano/greenshop/util/JwtUtil.java
@@ -1,18 +1,49 @@
 package com.matheusgermano.greenshop.util;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.matheusgermano.greenshop.models.User;
 import org.springframework.stereotype.Service;
+
+import java.security.SignatureException;
 
 @Service
 public class JwtUtil {
     public String generateToken(User user) {
-        Algorithm algorithm = Algorithm.HMAC256(String.valueOf(user.getId()));
+        Algorithm algorithm = Algorithm.HMAC256("green@shop");
         return JWT.create()
                 .withIssuer("auth0")
                 .withSubject(user.getName())
                 .withClaim("id", String.valueOf(user.getId()))
                 .sign(algorithm);
+    }
+
+    public boolean isValid(String token) {
+        try {
+            DecodedJWT jwt = JWT.decode(token);
+            Algorithm algorithm = Algorithm.HMAC256("green@shop");
+            JWTVerifier verifier = JWT.require(algorithm).build();
+            verifier.verify(token);
+
+            return true;
+        } catch (JWTVerificationException exception){
+            return false;
+        }
+    }
+
+    public String getTokenIdClaim(String token) {
+        try {
+            DecodedJWT jwt = JWT.decode(token);
+            String tokenIdClaim = jwt.getClaim("id").asString();
+
+
+            return tokenIdClaim;
+        } catch (JWTDecodeException exception){
+            return "";
+        }
     }
 }
