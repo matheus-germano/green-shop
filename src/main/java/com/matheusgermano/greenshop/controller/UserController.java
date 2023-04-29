a585ff8df810bb7735ea7a5a1fe6994ffba403ca
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-04-28 23:37:08 -0300

    :sparkles: feat: sign in method in user controller

--- src/main/java/com/matheusgermano/greenshop/controller/UserController.java
+++ src/main/java/com/matheusgermano/greenshop/controller/UserController.java
@@ -2,6 +2,7 @@ package com.matheusgermano.greenshop.controller;
 
 import com.matheusgermano.greenshop.models.User;
 import com.matheusgermano.greenshop.repositories.UsersRepository;
+import com.matheusgermano.greenshop.util.JwtUtil;
 import com.matheusgermano.greenshop.util.PasswordUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,22 +12,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.NoSuchAlgorithmException;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
     private UsersRepository usersRepository;
     private PasswordUtil passwordUtil;
+    private JwtUtil jwtUtil;
 
-    public UserController(UsersRepository usersRepository, PasswordUtil passwordUtil) {
+    public UserController(UsersRepository usersRepository, PasswordUtil passwordUtil, JwtUtil jwtUtil) {
         this.usersRepository = usersRepository;
         this.passwordUtil = passwordUtil;
+        this.jwtUtil = jwtUtil;
     }
 
-    @PostMapping
-    public ResponseEntity<User> store(@RequestBody User user) throws NoSuchAlgorithmException {
+    @PostMapping("/sign-in")
+    public ResponseEntity<String> signIn(@RequestBody Map<String, String> signInData) {
+        try {
+            String encryptedPassword = passwordUtil.encrypt(signInData.get("password"));
+            User userExists = usersRepository.findByEmailAndPassword(signInData.get("email"), encryptedPassword);
+
+            if (userExists == null) {
+                return new ResponseEntity("E-mail or password invalid", HttpStatus.BAD_REQUEST);
+            }
+
+            String generatedToken = jwtUtil.generateToken(userExists);
+            HashMap<String, String> token = new HashMap<>();
+            token.put("token", generatedToken);
+
+            return ResponseEntity.ok(generatedToken);
+        } catch (Exception e) {
+            return (ResponseEntity<String>) ResponseEntity.internalServerError();
+        }
+    }
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<User> signUp(@RequestBody User user) throws NoSuchAlgorithmException {
         try {
             User inCountryIdAlreadyExists = usersRepository.findByInCountryId(user.getInCountryId());
             User emailAlreadyExists = usersRepository.findByEmail(user.getEmail());
