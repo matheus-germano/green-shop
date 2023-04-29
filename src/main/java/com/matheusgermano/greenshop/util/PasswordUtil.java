4233cc6ca39ae67006f6e7fb6844250681db7c56
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-04-28 22:06:15 -0300

    :sparkles: feat: password SHA256 encrypt

---
+++ src/main/java/com/matheusgermano/greenshop/util/PasswordUtil.java
@@ -0,0 +1,22 @@
+package com.matheusgermano.greenshop.util;
+
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@Service
+public class PasswordUtil {
+    public String encrypt(String passwordToEncrypt) throws NoSuchAlgorithmException {
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        byte[] hashedPasswordBytes = messageDigest.digest(passwordToEncrypt.getBytes(StandardCharsets.UTF_8));
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (byte b : hashedPasswordBytes) {
+            stringBuilder.append(String.format("%02x", b));
+        }
+
+        return stringBuilder.toString();
+    }
+}
