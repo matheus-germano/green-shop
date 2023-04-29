3fc34d80447c4fb8c9bbbc704b2bd957a58aa398
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-04-28 21:21:21 -0300

    :sparkles: feat: db migration v1

--- src/main/resources/db/migration/V1__create-users-table.sql
+++ src/main/resources/db/migration/V1__create-users-table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users(
     id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
-    inCountryId VARCHAR(11) NOT NULL UNIQUE,
+    in_country_id VARCHAR(11) NOT NULL UNIQUE,
     name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
